c78aa4f22f94489880ebde68244b7e741f4dacaf
Ganesh Sahu <sahuliferay@gmail.com>
2023-03-08 01:15:32 +0530

    Create AsyncService.java

---
+++ asyncio/src/main/java/com/aysnc/asyncio/AsyncService.java
@@ -0,0 +1,24 @@
+package com.rest.async.rest;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class AsyncService {
+
+
+    private final RestTemplate restTemplate;
+
+    public AsyncService (RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Async
+    public CompletableFuture<String> getDataAsync() {
+        String result = restTemplate.getForObject("https://ipinfo.io/161.185.160.93/geo", String.class);
+        return CompletableFuture.completedFuture(result);
+    }
+}
