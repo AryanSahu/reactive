b3ba56ee87b6e05c2742e58230a8984886962a8a
Ganesh Sahu <sahuliferay@gmail.com>
2023-03-08 01:14:36 +0530

    Update AsyncExternalCall.java

--- asyncio/src/main/java/com/aysnc/asyncio/AsyncExternalCall.java
+++ asyncio/src/main/java/com/aysnc/asyncio/AsyncExternalCall.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import java.util.concurrent.CompletableFuture;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @RestController
 public class AsyncExternalCall {
