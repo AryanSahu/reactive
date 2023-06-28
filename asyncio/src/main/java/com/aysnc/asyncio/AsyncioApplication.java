9e6beda474f0ff1606eab7a73271c8df4fa9ddfe
Ganesh Sahu <sahuliferay@gmail.com>
2023-06-28 20:11:40 +0530

    Update AsyncioApplication.java
    test review

--- asyncio/src/main/java/com/aysnc/asyncio/AsyncioApplication.java
+++ asyncio/src/main/java/com/aysnc/asyncio/AsyncioApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AsyncioApplication {
 
 	// this file will be reviewed as part of the demo
+	int[] numbers = {1, 2, 3};
+        System.out.println(numbers[4]);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AsyncioApplication.class, args);
 	}
