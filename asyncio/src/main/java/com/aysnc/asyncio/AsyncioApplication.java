79202024ea1ad54a61ffb06a6059823f986223a2
Ganesh Sahu <sahuliferay@gmail.com>
2023-06-28 17:42:30 +0530

    Update AsyncioApplication.java

--- asyncio/src/main/java/com/aysnc/asyncio/AsyncioApplication.java
+++ asyncio/src/main/java/com/aysnc/asyncio/AsyncioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAsync
 public class AsyncioApplication {
 
+	// this file will be reviewed as part of the demo
 	public static void main(String[] args) {
 		SpringApplication.run(AsyncioApplication.class, args);
 	}
