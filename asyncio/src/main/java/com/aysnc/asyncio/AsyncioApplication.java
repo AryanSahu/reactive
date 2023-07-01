5f1f0c3a43b7a0d35de0b7f7161b0c4b8655ecc0
Ganesh Sahu <sahuliferay@gmail.com>
2023-07-01 14:58:01 +0530

    Update AsyncioApplication.java

--- asyncio/src/main/java/com/aysnc/asyncio/AsyncioApplication.java
+++ asyncio/src/main/java/com/aysnc/asyncio/AsyncioApplication.java
@@ -9,7 +9,9 @@ public class AsyncioApplication {
 
 	public static void main(String[] args) {
 
-		
+		String request;
+		String username = request.getParameter("username");
+                String query = "SELECT * FROM users WHERE username = '" + username + "'";
 		
 		SpringApplication.run(AsyncioApplication.class, args);
 	}
